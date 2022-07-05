6791072a2197ac4624132daabd55f18ba948f794
Tommygithubaccount123 <stinkysocks13@gmail.com>
2022-07-05 11:23:48 -0700

    print array

--- finished/maximum-subarray.cpp
+++ finished/maximum-subarray.cpp
@@ -1,18 +1,44 @@
-#KADANES ALGO
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        if (!n){
-            return 0;
-        }
-        int out = INT_MIN;
-        int sum = 0;
-        for (int i=0; i<n; i++){
-            sum = max(nums[i], sum+nums[i]);
-            out = max(out,sum);
-        }
-         
-        return out;
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//KADANES ALGO
+int maxSubArray(vector<int>& nums) {
+    int n = nums.size();
+    if (!n){
+        return 0;
     }
-};
+    int max_out = nums[0];
+    int current_max = nums[0];
+    int start_index_sum = 0;
+    int end_index = 0;
+    int start_index = 0;
+    for (int i=1; i<n; i++){
+        //current_max = max(nums[i], current_max+nums[i]);
+        if (current_max+nums[i]>nums[i]){
+            current_max += nums[i];
+        }
+        else{
+            current_max = nums[i];
+            start_index_sum = i;
+        }
+        //max_out = max(max_out,current_max);
+        if (current_max > max_out){
+            max_out = current_max;
+            start_index = start_index_sum;
+            end_index = i;
+        }
+    }
+    cout << "[";
+    for (int i=start_index; i<=end_index; i++){
+        cout << nums[i] << ",";
+    }
+    cout << "]" << endl;
+    return max_out;
+    }
+
+int main(){
+    vector<int> test = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << maxSubArray(test) << endl;
+}
