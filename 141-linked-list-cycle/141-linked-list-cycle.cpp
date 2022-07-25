7b45af6efdc16f471ff5e4af8ed727ea42533e09
Thomas So <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-07-24 22:28:04 -0700

    Time: 4 ms (99.20%), Space: 8.1 MB (57.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,14 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int> storage;
-        while(head){
-            if (storage.find(head)==storage.end()){
-                storage[head] = 0;
-            } else {
-                return true;
-            }
-            head = head->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow==fast){return true;}
         }
         
         return false;
