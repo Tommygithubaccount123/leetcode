55cf5ef94d5112da7ee1b0c30ae70b99ea4d16f1
Thomas So <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-07-26 18:08:15 -0700

    Time: 23 ms (57.07%), Space: 8.9 MB (40.46%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -3,16 +3,12 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> storage;
         for (auto i : magazine){
-            if (storage.find(i) == storage.end()){
-                storage[i] = 0;
-            } else {
-                storage[i] += 1;
-            }
+            storage[i]++;
         }
         
         for (auto i : ransomNote){
             if (storage.find(i)!=storage.end()){
-                if (storage[i] < 0){return false;}
+                if (storage[i] < 1){return false;}
                 storage[i] -= 1;
             } else {
                 return false;
