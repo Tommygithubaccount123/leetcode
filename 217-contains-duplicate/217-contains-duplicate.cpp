7137d56c7f0fccbb0009f2a69696499fd78a4fed
Thomas So <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-08-03 12:01:56 -0700

    Time: 103 ms (91.44%), Space: 51.4 MB (63.63%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> storage;
+        unordered_map<int,bool> storage;
         for (auto& i : nums){
             if (storage.find(i)!=storage.end()){
                 return true;
             }
-            storage[i] = 1;
+            storage[i] = true;
         }
         
         return false;
