18c999f3df2fdacc54305e18a1f3bb0ef6f61eb3
Thomas So <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-08-24 12:04:17 -0700

    Time: 204 ms (9.69%), Space: 43.2 MB (86.61%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class TrieNode {
+  public:
+    bool endofWord;
+    unordered_map<char, TrieNode*> children;
+    TrieNode(){
+        endofWord = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root = nullptr;
+    Trie() {
+        root = new TrieNode();
+    }
+    // ~Trie(){
+    //     destruct(root);
+    // }
+    // void destruct(TrieNode* node){
+    //     if (node->children.size()==0){delete node;}
+    //     for (auto child : node->children){
+    //         destruct(child.second);
+    //     }
+    //     delete node;
+    // }
+    
+    void insert(string word) {
+        TrieNode* cur = root;
+        for (auto letter : word){
+            if (cur->children.find(letter)==cur->children.end()){
+                cur->children[letter] = new TrieNode();
+                cur = cur->children[letter];
+            } else {
+                cur = cur->children[letter];
+            }
+        }
+        cur->endofWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* cur = root;
+        for (auto letter : word){
+            if (cur->children.find(letter)==cur->children.end()){
+                return false;
+            }
+            cur = cur->children[letter];
+        }
+        return cur->endofWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* cur = root;
+        for (auto letter : prefix){
+            if (cur->children.find(letter)==cur->children.end()){
+                return false;
+            }
+            cur = cur->children[letter];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
