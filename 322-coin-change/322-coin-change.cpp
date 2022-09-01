06ab5f8660353996c2d602ee61ffee30b206bfcf
Thomas So <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-09-01 15:11:17 -0700

    Time: 108 ms (75.97%), Space: 14.5 MB (55.45%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if (coins.size()==0 || amount==0){return 0;}
-        sort(coins.begin(), coins.end());
         vector<int> dp(amount+1, amount+1);
         dp[0] = 0;
+        sort(coins.begin(), coins.end());
         
         for (int i=1; i<=amount; i++){
-            // dp[i] = amount + 1;
             for (int j=0; j<coins.size(); j++){
-                if (coins[j] <= i){
-                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]); 
-                } else{
-                    break; 
+                if (i >= coins[j]){
+                    dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
+                } else {
+                    break;
                 }
             }
         }
-        return dp[amount] < amount + 1 ? dp[amount] : -1;
+        
+        return dp[amount]!= amount+1 ? dp[amount] : -1;
     }
 };
