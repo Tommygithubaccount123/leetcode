db37ee48e4b98a88ed5d81641701250355de5cdc
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2022-12-05 01:51:05 -0800

    Time: 4 ms (24.37%), Space: 6.4 MB (90.45%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,8 +4,8 @@ public:
         if (numRows == 1){return {{1}};}
         if (numRows == 2){return {{1},{1,1}};}
         // if (numRows == 3){return {{1},{1,1},{1,2,1}};}
-        vector<vector<int>> ans = {{1},{1,1},{1,2,1}};
-        for (int i=2; i<numRows-1; i++){
+        vector<vector<int>> ans = {{1},{1,1}};
+        for (int i=1; i<numRows-1; i++){
             vector<int> newRow = {1};
             for (int j=0; j<ans[i].size()-1; j++){
                 newRow.push_back(ans[i][j] + ans[i][j+1]);
