e2e093b819b7de3059209c9f931507c3afff9eb3
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-06-11 23:02:30 -0700

    Time: 10 ms (38.81%), Space: 14.2 MB (5.75%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,13 +6,15 @@ public:
         big[0] = nums[0];
         small[0] = nums[0];
         for (int i=1;i<nums.size();i++){
-            if(nums[i] < 0) {
-                big[i] = max(small[i-1]*nums[i],nums[i]);
-                small[i] = min(big[i-1]*nums[i],nums[i]);
-            } else {
-                big[i] = max(big[i-1]*nums[i],nums[i]);
-                small[i] = min(small[i-1]*nums[i],nums[i]);
-            }
+            // if(nums[i] < 0) {
+            //     big[i] = max(small[i-1]*nums[i],nums[i]);
+            //     small[i] = min(big[i-1]*nums[i],nums[i]);
+            // } else {
+            //     big[i] = max(big[i-1]*nums[i],nums[i]);
+            //     small[i] = min(small[i-1]*nums[i],nums[i]);
+            // }
+            big[i] = max(big[i-1]*nums[i], max(small[i-1]*nums[i],nums[i]));
+            small[i] = min(small[i-1]*nums[i], min(big[i-1]*nums[i],nums[i]));
         }
         return *max_element(big.begin(),big.end());
     }
