424f241f71a6778c4ad9f255b0258d711df0e686
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-07-26 12:06:36 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (13.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    vector<int> storage = {1,2};
-    int climbStairs(int n){
-        if (n == 1){return 1;}
-        if (n == 2){return 2;}
-        for (int i=2; i<n; i++){
-            storage.push_back(storage[i-2] + storage[i-1]);
+    int climbStairs(int n) {
+        if (n<2){
+            return n;
         }
-        return storage[storage.size()-1];
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
