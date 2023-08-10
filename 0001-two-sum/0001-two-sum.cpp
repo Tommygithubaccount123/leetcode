66010e15a69439783571fbd12bd874b53381ee28
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-08-10 10:45:02 -0700

    Time: 11 ms (81.28%), Space: 10.8 MB (43.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> storage;
+        int diff;
         for (int i=0; i<nums.size(); i++){
-            if (storage.find(target - nums[i]) != storage.end()){
-                return {i, storage[target - nums[i]]};
+            diff = target - nums[i];
+            if (storage.find(diff) != storage.end()){
+                return {storage[diff],i};
             }
             storage[nums[i]] = i;
         }
