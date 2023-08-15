183b369da9360f037ecbc5db809f391ad871b696
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-08-15 12:01:06 -0700

    Time: 126 ms (71.81%), Space: 67.6 MB (28.76%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if (n==0){return 0;}
+        
+        int big = nums[0];
         unordered_map<int,int> storage;
         for (auto num : nums){
             storage[num]++;
+            big = max(big,num);
         }
         
-        int result = 0;
-        for (auto num : nums){
-            if (storage.find(num-1) == storage.end()){
-                int len = 0;
-                int next = num;
-                while (true){
-                    if (storage.find(next) != storage.end()){
-                        len++;
-                        next++;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                result = max(result, len);
+        
+        vector<int> lefts;
+        for (auto elem : storage){
+            if (storage.find(elem.first-1) == storage.end()){
+                lefts.push_back(elem.first);
             }
         }
-        return result;
+        
+        int result = 1;
+        int cur = 1;
+        for (auto left : lefts){
+            cur = 1;
+            for (int i=left+1; i<big+1; i++){
+                if (storage.find(i) != storage.end()){
+                    cur++;
+                } else {
+                    break;
+                }
+            }
+            result = max(result,cur);
+        }
+        return max(cur,result);
     }
 };
