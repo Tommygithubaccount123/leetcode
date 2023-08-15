26b73e6ee022a0b5ddd78247f0a3ed836bc8cb0b
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-08-15 14:35:20 -0700

    Time: 4 ms (73.17%), Space: 7.2 MB (87.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,13 +3,12 @@ public:
     bool isPalindrome(string s) {
         int l = 0;
         int r = s.size()-1;
-        while(l < r){
+        while(l <= r){
             if (s[l] == ' ' || !isalnum(s[l])){
                 l++;
             }
             else if (s[r] == ' ' || !isalnum(s[r])){
                 r--;
-                continue;
             }
             else if (tolower(s[l]) == tolower(s[r])){
                 l++;
