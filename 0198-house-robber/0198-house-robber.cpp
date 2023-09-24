5b5ed64ece9679653d906a87393945d69d59f0a2
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-09-24 15:18:11 -0700

    Time: 0 ms (100.00%), Space: 8.1 MB (8.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,7 +8,7 @@ public:
         dp[1] = max(nums[0],nums[1]);
         
         for (int i=2; i<n; i++){
-            dp[i] = max(dp[i-2] + dp[i], dp[i-1]);
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
         }
         return dp[n-1];
     }
