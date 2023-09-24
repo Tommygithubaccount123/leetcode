58a1911669ad3a701c70c21da7a5b2a9b6b2644f
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-09-24 15:18:01 -0700

    Time: 3 ms (36.98%), Space: 8.1 MB (8.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        //dp[i] stores max profit up to house i
         vector<int> dp = nums;
-        int n = nums.size();
+        int n = dp.size();
+        if (n == 1){return nums[0];}
+        if (n == 2){return max(nums[0],nums[1]);}
+        dp[1] = max(nums[0],nums[1]);
         
-        //base cases
-        if (n == 1) {return nums[0];}
-        if (n == 2) {return nums[0] > nums[1] ? nums[0] : nums[1];}
-        dp[1] = max(dp[0], dp[1]);
-        
-        //fill dp table
-        for (int i=2; i<nums.size(); i++){
-            dp[i] = max(dp[i-1], dp[i] + dp[i-2]);
+        for (int i=2; i<n; i++){
+            dp[i] = max(dp[i-2] + dp[i], dp[i-1]);
         }
         return dp[n-1];
     }
