bfc38c4df648b2ed03dd4bffe7519976f01f2847
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2024-01-14 14:18:22 -0800

    Time: 7 ms (53.52%), Space: 21.1 MB (25.51%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -5,6 +5,7 @@ public:
         for (int i=1; i<mountain.size()-1; i++){
             if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]){
                 result.push_back(i);
+                i++;
             }
         }
         return result;
