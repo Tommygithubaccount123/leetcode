33f9acb6063ef2c748a6414da8095d6ae4686735
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-03-08 16:33:46 -0800

    Time: 10 ms (14.94%), Space: 18.9 MB (25.67%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,16 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.storage = dict()
+
     def depth(self, root):
         if not root:
+            self.storage[root] = 0
             return 0
-        return max(self.depth(root.left), self.depth(root.right)) + 1
+        math = max(self.depth(root.left), self.depth(root.right)) + 1
+        self.storage[root] = math
+        return math
 
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    def work(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        left = self.depth(root.left)
-        right = self.depth(root.right)
+        left = self.storage[root.left]
+        right = self.storage[root.right]
         if abs(left-right) > 1:
             return False
-        return self.isBalanced(root.right) and self.isBalanced(root.left)
+        return self.work(root.right) and self.work(root.left)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.depth(root)
+        return self.work(root)
