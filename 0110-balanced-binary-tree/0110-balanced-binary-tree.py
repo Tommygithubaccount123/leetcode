90c12150e0e5f4497928e92dfc8cd3ca5eaf4bfe
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-17 16:54:47 -0700

    Time: 7 ms (53.54%), Space: 17.1 MB (86.22%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,30 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution(object):
     def __init__(self):
-        self.storage = dict()
+        self.result = True
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        self.do(root)
+        return self.result
 
-    def depth(self, root):
-        if not root:
-            self.storage[root] = 0
+    def do(self, node):
+        if not node:
             return 0
-        math = max(self.depth(root.left), self.depth(root.right)) + 1
-        self.storage[root] = math
-        return math
+        
+        left = self.do(node.left)
+        right = self.do(node.right)
 
-    def work(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        left = self.storage[root.left]
-        right = self.storage[root.right]
-        if abs(left-right) > 1:
-            return False
-        return self.work(root.right) and self.work(root.left)
+        if abs(left - right) > 1:
+            self.result = False
+        return max(left, right) + 1
 
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.depth(root)
-        return self.work(root)
+
+
