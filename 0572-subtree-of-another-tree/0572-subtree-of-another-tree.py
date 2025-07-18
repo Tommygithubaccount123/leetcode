ae3c4713edde58d0dc10b21f59e90c0829c61037
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-17 17:22:23 -0700

    Time: 79 ms (55.73%), Space: 13.4 MB (77.12%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,11 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root and not subRoot:
+            return True
+        if not root:
+            return False
+        same = self.isSameTree(root, subRoot)
+        if same:
+            return True
+
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+
+        return left or right
+
+
+    def isSameTree(self, p, q):
         if not p and not q:
             return True
         if not p or not q:
@@ -13,13 +33,8 @@ class Solution:
         
         if p.val != q.val:
             return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
 
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if subRoot is None:
-            return True
-        if root is None:
-            return False
-        if self.isSameTree(root, subRoot):
-            return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)       
+        return left and right
