e3690e161dc0b185c0d5fa24c752ebccea1d37c5
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-17 23:05:58 -0700

    Time: 44 ms (93.49%), Space: 20.7 MB (7.61%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,38 +7,13 @@
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        x, y, found = self.do(root, p, q)
-        return found
-
-    def do(self, node, p, q, found=None):
-        if not node:
-            return False, False, None
-        if found:
-            return True, True, found
-
-        q_found = p_found = False
-        if node.val == p.val:
-            p_found = True
-        if node.val == q.val:
-            q_found = True
-
-        l_p, l_q, found_l = self.do(node.left, p, q)
-        r_p, r_q, found_r = self.do(node.right, p, q)
-
-        if found_l:
-            return True, True, found_l
-        if found_r:
-            return True, True, found_r
-        if p_found and (l_q or r_q):
-            return True, True, node
-        if q_found and (l_p or r_p):
-            return True, True, node
-        if (l_p and r_q) or (l_q and r_p):
-            return True, True, node
+        if not root:
+            return
         
-        return p_found or (l_p or r_p), q_found or (l_q or r_q), None
-
-
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
         
-
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
         
+        return root
