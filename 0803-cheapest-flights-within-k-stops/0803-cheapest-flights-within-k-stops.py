a0e1db32315a0a6266425641058e3fbdd4714a35
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-21 00:35:30 -0700

    Time: 6 ms (82.8%), Space: 13.4 MB (54.23%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+        n = len(flights)
+        if n == 0:
+            return -1
+
+        cost = dict()
+        adj_list = dict()
+        for source, dest, price in flights:
+            if source in adj_list:
+                adj_list[source].append((dest, price))
+            else:
+                adj_list[source] = [(dest,price)]
+            if dest not in cost:
+                cost[dest] = float("inf")
+        cost[src] = 0
+
+        queue = [(src,0)]
+        count = 0
+        while (queue and count <= k):
+            length = len(queue)
+            for i in range(length):
+                flight, cur_cost = queue.pop(0)
+                if flight in adj_list:
+                    for elem in adj_list[flight]:
+                        dest, price = elem
+                        new_price = price + cur_cost
+                        if new_price < cost[dest]:
+                            cost[dest] = new_price
+                            queue.append((dest, new_price))
+            count += 1
+        if dst not in cost or cost[dst] == float("inf"):
+            return -1
+        return cost[dst]
+
+
+
