f3328836996e05811d831acfb22b0936f40f709d
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-22 15:53:10 -0700

    Time: 54 ms (68.8%), Space: 19.6 MB (10.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,16 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        small = 10**4
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy = prices[0]
         profit = 0
-        for price in prices:
-            small = min(small, price)
-            profit = max(profit, price-small)
+
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+            
         return profit
