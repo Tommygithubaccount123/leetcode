2265b97c9908f19ecd26aaf3655c4fe553c51c6c
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-23 21:01:42 -0700

    Time: 6001 ms (5.62%), Space: 14.7 MB (41.57%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
@@ -8,19 +8,15 @@ class Solution(object):
         ks = nums.count(k)
 
         glob_max = 0
-        for i in range(1,51): # Simulate changing the nums (1-50) to k
-            if i == k:
-                continue
-            cur_max = counter = 0
+        for i in range(1,51):
+            counter = 0
+            cur_max = 0
             for num in nums:
-                if i == num:
-                    counter += 1
-                elif num == k: # subtract k since we are trying to convert as many # into k
+                if num == k:
                     counter -= 1
+                if num == i:
+                    counter += 1
                 counter = max(counter, 0)
                 cur_max = max(cur_max, counter)
             glob_max = max(glob_max, cur_max)
         return glob_max + ks
-
-
-            
