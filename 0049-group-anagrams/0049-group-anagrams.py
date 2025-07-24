b08b69d476b8c06fb1ed7b35851a9ded7aa4c392
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-23 23:46:55 -0700

    Time: 12 ms (60.47%), Space: 20.4 MB (98.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        storage = dict()
-
-        for word in strs:
-            key = ''.join(sorted(word))
-            if key not in storage:
-                storage[key] = [word]
+        storage = {}
+        n = len(strs)
+        out = []
+        for i in range(n):
+            word = strs[i]
+            sort = "".join(sorted(word))
+            if sort not in storage:
+                storage[sort] = [word]
             else:
-                storage[key].append(word)
-        
+                storage[sort].append(word)
         out = []
         for elem in storage:
             out.append(storage[elem])
         return out
+
