2b0179f50b65d49592bcab02f9ff8a550818c31f
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-23 22:08:13 -0700

    Time: 9 ms (67.66%), Space: 14.4 MB (43.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,22 +8,16 @@ class Solution(object):
         storage = {}
         for num in nums:
             if num in storage:
-                storage[num] += 1
+                storage[num] -= 1
             else:
-                storage[num] = 1
+                storage[num] = -1
         
         heap = []
         for num, freq in storage.items():
-            if heap and len(heap) >= k:
-                tempFreq, tempNum = heap[0]
-                if tempFreq < freq:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, (freq,num))
-            else:
-                heapq.heappush(heap, (freq,num))
+            heapq.heappush(heap, (freq,num))
         
         result = []
-        while (heap):
+        for i in range(k):
             freq, num = heapq.heappop(heap)
             result.append(num)
         return result
