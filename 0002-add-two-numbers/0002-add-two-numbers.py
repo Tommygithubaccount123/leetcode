1108c962c05d200bf29ba3b48b2fb04e9fe052b4
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-23 21:34:26 -0700

    Time: 11 ms (37.59%), Space: 12.5 MB (84.2%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,33 +10,24 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        first = 0
-        counter = 0
-        while (l1):
-            first += l1.val * 10**counter
-            l1 = l1.next
-            counter += 1
-        second = 0
-        counter = 0
-        while (l2):
-            second += l2.val * 10**counter
-            l2 = l2.next
-            counter += 1
+        dummy = ListNode(None)
+        tail = dummy
+        carry = 0
 
-        total = first + second
-        print(first, second)
-        if total == 0:
-            return ListNode(0)
-        prev = None
-        head = None
+        counter = 0
+        while (l1 or l2 or carry):
+            total = carry
 
-        while (total):
-            node_val = total % 10
-            total /= 10
-            node = ListNode(node_val)
-            if prev:
-                prev.next = node
-            else:
-                head = node
-            prev = node
-        return head
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
+        return tail.next 
+            
