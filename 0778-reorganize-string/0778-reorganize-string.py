89216f265d4924c2e6c7d6733e7d18a4923cb59b
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-07-27 17:08:13 -0700

    Time: 0 ms (100%), Space: 18 MB (27.96%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,50 +1,32 @@
-class Solution(object):
-    def reorganizeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        n = len(s)
-        storage = {}
-        for i in range(n):
-            if s[i] not in storage:
-                storage[s[i]] = 1
-            else:
-                storage[s[i]] += 1
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        storage = defaultdict(int)
+        for i in s:
+            storage[i] += 1
         
-        max_heap = []
-        for letter in storage:
-            freq = -storage[letter]
-            heapq.heappush(max_heap, (freq, letter))
-        result = ""
-
-        while (len(max_heap) >= 2):
-            freq1, letter1 = heapq.heappop(max_heap)
-            freq2, letter2 = heapq.heappop(max_heap)
-            freq1 *= -1
-            freq2 *= -1
-
-            if freq1 > 1:
-                freq1 -= 1
-                freq1 *= -1
-                heapq.heappush(max_heap, (freq1, letter1))
-            if freq2 > 1:
-                freq2 -= 1
-                freq2 *= -1
-                heapq.heappush(max_heap, (freq2, letter2))
-            
-            result += letter1+letter2
+        heap = []
+        for key, value in storage.items():
+            pair = [-value, key]
+            heapq.heappush(heap, pair)
         
-        if max_heap:
-            freq, letter = heapq.heappop(max_heap)
-            freq *= -1
-            if freq > 1 :
+        out = ""
+        while len(heap) > 1:
+            first = heapq.heappop(heap)
+            second = heapq.heappop(heap)
+
+            out += first[1] + second[1]
+            first[0] += 1
+            second[0] += 1
+            if -first[0] > 0:
+                heapq.heappush(heap, first)
+            if -second[0] > 0:
+                heapq.heappush(heap, second)
+        
+        if heap:
+            last = heapq.heappop(heap)
+            if -last[0] > 1:
                 return ""
-            else:
-                result += letter
-        return result
+            return out + last[1]
+        return out
             
-            
-
- 
 
