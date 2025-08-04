5e0187994b92e4bfdbac1eb560189b6c12ff5641
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-08-04 13:15:21 -0700

    Time: 135 ms (41.68%), Space: 74.6 MB (58.18%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,32 +1,27 @@
 class TimeMap:
 
     def __init__(self):
-        self.storage = defaultdict(list) # {"foo" : [(bar,1), (bar2, 4)...]}
+        self.storage = defaultdict(list) # {key : [(val, time)...], ...}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         pair = (value, timestamp)
         self.storage[key].append(pair)
 
     def get(self, key: str, timestamp: int) -> str:
-        if not self.storage[key]:
+        if key not in self.storage:
             return ""
-        times = self.storage[key]
+        values = self.storage[key]
         l = 0
-        r = len(times) - 1
-        mid = 0
-        best_guess = ""
+        r = len(values) - 1
+        ans = ""
         while (l<=r):
-            mid = (r-l)//2 + l
-            guess = times[mid]
-            if guess[1] <= timestamp:
+            mid = (r-l) // 2 + l
+            if values[mid][1] <= timestamp:
+                ans = values[mid][0]
                 l = mid + 1
-                best_guess = guess[0]
-            elif guess[1] > timestamp:
+            else:
                 r = mid - 1
-        return best_guess
-
-
-        
+        return ans
 
 
 # Your TimeMap object will be instantiated and called as such:
