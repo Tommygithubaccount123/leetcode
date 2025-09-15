c3deac000a9b0df90ffa2d7b371047213f5a3c1c
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-09-14 22:19:18 -0700

    Time: 0 ms (100%), Space: 17.8 MB (95.68%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,35 +1,27 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def zigzagLevelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
-        if not root:
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
             return []
         
-        queue = [root]
-        result = []
-        left_to_right = 1
-        while (queue):
-            n = len(queue)
+        isZig = False
+        levels = []
+        q = deque()
+        q.append(root)
+        while q:
             level = []
-            for i in range(n):
-                node = queue.pop(0)
-                if node:
-                    level.append(node.val)
-                    queue.append(node.left)
-                    queue.append(node.right)
-            if level:
-                if left_to_right:
-                    result.append(level)
-                else:
-                    result.append(level[::-1])
-            left_to_right = not left_to_right
-        return result
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            if isZig:
+                level.reverse()
+            
+            levels.append(level)
+            isZig = not isZig
 
+        return levels
