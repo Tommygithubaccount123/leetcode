0e286c3b1ea532e920836917c750574816d80bdb
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-09-25 16:02:53 -0700

    Time: 272 ms (27.67%), Space: 20.4 MB (42.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,22 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        self.m = len(grid)
-        self.n = len(grid[0])
-        result = 0
-        self.visited = {}
-        
-        self.dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+class Solution:
+    def __init__(self):
+        self.dirs = [(1,0), (-1,0), (0, 1), (0, -1)]
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                ans += self.helper(i, j, grid)
+        return ans
 
-        for i in range(self.m):
-            for j in range(self.n):
-                result += self.dfs(i, j, grid)
-        return result
-
-    def dfs(self, x, y, grid):
-        if x < 0 or y < 0 or x >= self.m or y >= self.n:
+    def helper(self, i, j, grid):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
             return 0
-        if (x,y) in self.visited:
+        spot = grid[i][j]
+        if spot != "1":
             return 0
-        if grid[x][y] == "0" or grid[x][y] == "2":
-            return 0
-
-        self.visited[(x,y)] = True
-        for dx,dy in self.dirs:
-            self.dfs(x+dx,y+dy, grid)
+        grid[i][j] = "2"
+        for direction in self.dirs:
+            self.helper(i + direction[0], j + direction[1], grid)
         return 1
         
+
